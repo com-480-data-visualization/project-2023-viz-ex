c2b6d7a93d733a18f6c5329205bd5caca4525ad5
Olivier Saint-Cyr <saintcyr.olivier1@gmail.com>
2023-05-01 22:58:37 +0200

    init webpack

---
+++ project/src/main.js
@@ -0,0 +1,10 @@
+import * as d3 from "d3-drag"
+
+
+function component() {
+    const element = document.createElement('div');
+ 
+    return element;
+  }
+ 
+  document.body.appendChild(component());
---
+++ project/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: './src/main.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+        template: 'src/index.html'
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: "src/data/*",
+                    to({ context, absoluteFilename }) {
+                        return "data/[name][ext]";
+                    }
+                },
+                { 
+                    from: "src/*.css", 
+                    to({ context, absoluteFilename }) {
+                        return "[name][ext]";
+                    } 
+                },
+            ],
+        })
+    ],
+    devServer: {
+        static: {
+        directory: path.join(__dirname, 'dist'),
+        },
+        port: 8000,
+    }
+};
