3530f9aae0db726b18a27ab4f9a14937aa235f3d
Olivier Saint-Cyr <saintcyr.olivier1@gmail.com>
2023-05-02 12:44:38 +0200

    basic map

--- project/src/main.js
+++ project/src/main.js
@@ -1,10 +1,26 @@
-import * as d3 from "d3-drag"
+import L from 'leaflet'
 
+const whenDocumentLoaded = (action) => {
+	if (document.readyState === "loading") {
+		document.addEventListener("DOMContentLoaded", action);
+	} else {
+		// `DOMContentLoaded` already fired
+		action();
+	}
+}
 
-function component() {
-    const element = document.createElement('div');
- 
-    return element;
-  }
- 
-  document.body.appendChild(component());
+whenDocumentLoaded(() => {
+    if (!window.isScriptLoaded) {
+        console.log("asdfasdf")
+        const mapObj = L.map('map').setView([51.505, -0.09], 13);
+
+        const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attributionControl:false
+        }).addTo(mapObj);
+        
+        window.isScriptLoaded= true;
+    }
+    
+	// plot object is global, you can inspect it in the dev-console
+});
