6a980e0c460d670b693177175faeb687f8b2f4fd
Olivier Saint-Cyr <saintcyr.olivier1@gmail.com>
2023-05-02 14:07:13 +0200

    load map data

--- project/src/main.js
+++ project/src/main.js
@@ -1,5 +1,6 @@
 import L from 'leaflet'
 import { select, selectAll } from 'd3-selection'
+import { csv } from 'd3'
 
 const initMap = () => {
     var map = L
@@ -18,41 +19,38 @@ const initMap = () => {
     return map;
 }
 
-const initD3MapLayer = (map) => {
-    // Create data for circles:
-    var markers = [
-        {long: 9.083, lat: 42.149}, // corsica
-        {long: 7.26, lat: 43.71}, // nice
-        {long: 2.349, lat: 48.864}, // Paris
-        {long: -1.397, lat: 43.664}, // Hossegor
-        {long: 3.075, lat: 50.640}, // Lille
-        {long: -3.83, lat: 48}, // Morlaix
-      ];
-      
-      // Select the svg area and add circles:
-      select("#mapid")
+const initD3MapLayer = (map, markers) => {
+    // Select the svg area and add circles:
+    select("#mapid")
         .select("svg")
         .selectAll("myCircles")
         .data(markers)
         .enter()
         .append("circle")
-          .attr("cx", function(d){ return map.latLngToLayerPoint([d.lat, d.long]).x })
-          .attr("cy", function(d){ return map.latLngToLayerPoint([d.lat, d.long]).y })
-          .attr("r", 14)
-          .style("fill", "red")
-          .attr("stroke", "red")
-          .attr("stroke-width", 3)
-          .attr("fill-opacity", .4)
+            .attr("cx", function(d){ return map.latLngToLayerPoint([d.lat, d.long]).x })
+            .attr("cy", function(d){ return map.latLngToLayerPoint([d.lat, d.long]).y })
+            .attr("r", 3)
+            .style("fill", "red")
+            .attr("stroke", "red")
+            .attr("stroke-width", 3)
+            .attr("fill-opacity", .4)
       
-      // Function that update circle position if something change
-      function update() {
-        selectAll("circle")
-          .attr("cx", function(d){ return map.latLngToLayerPoint([d.lat, d.long]).x })
-          .attr("cy", function(d){ return map.latLngToLayerPoint([d.lat, d.long]).y })
-      }
-      
-      // If the user change the map (zoom or drag), I update circle position:
-      map.on("moveend", update)
+    // Function that update circle position if something change
+    function update() {
+    selectAll("circle")
+        .attr("cx", function(d){ return map.latLngToLayerPoint([d.lat, d.long]).x })
+        .attr("cy", function(d){ return map.latLngToLayerPoint([d.lat, d.long]).y })
+    }
+    
+    // If the user change the map (zoom or drag), I update circle position:
+    map.on("moveend", update)
+}
+
+const loadData = (afterLoadCallback) => {
+    csv("/data/database.csv").then(data => {
+        console.log("Data Loaded")
+        afterLoadCallback(data)
+    })
 }
 
 const whenDocumentLoaded = (action) => {
@@ -66,7 +64,15 @@ const whenDocumentLoaded = (action) => {
 whenDocumentLoaded(() => {
     if (!window.isScriptLoaded) {
         let map = initMap()
-        initD3MapLayer(map)
-        window.isScriptLoaded= true;
+        loadData((data) => {
+            let latLongs = data.map((record) => {
+                return { 
+                    lat: parseInt(record.Latitude),
+                    long: parseInt(record.Longitude),
+                }
+            })
+            initD3MapLayer(map, latLongs)
+        })
+        window.isScriptLoaded = true;
     }
 });
