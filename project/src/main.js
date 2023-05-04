75efceaa3e2be48aca535edb1c1ead6ba8c4f72f
julie-annej <60227045+julie-annej@users.noreply.github.com>
2023-05-04 08:57:47 +0200

    choropleth map

--- project/src/main.js
+++ project/src/main.js
@@ -1,6 +1,6 @@
-import L, { bounds } from 'leaflet'
+import L, { bounds, geoJSON } from 'leaflet'
 import { select, selectAll } from 'd3-selection'
-import { csv, hierarchy } from 'd3'
+import { csv, hierarchy, json } from 'd3'
 
 const initMap = () => {
     let map = L
@@ -15,8 +15,6 @@ const initMap = () => {
 		maxZoom: 10,
 	}).addTo(map);
 	
-	// Add a svg layer to the map
-	L.svg().addTo(map);
     return map;
 }
 
@@ -35,9 +33,8 @@ const initD3MapLayer = (map, earthquakes) => {
         let min = minRadius(height)
         return (d.mag - 5.5)/(10 - 5.5) * (max - min) + min
     }
-
-    select("#mapid")
-        .select("svg")
+    var svgOverlayElement = select("#mapid")
+        .append("svg")
         .selectAll("myCircles")
         .data(earthquakes)
         .enter()
@@ -60,6 +57,7 @@ const initD3MapLayer = (map, earthquakes) => {
     
     // If the user change the map (zoom or drag), I update circle position:
     map.on("moveend", update)
+    return svgOverlayElement
 }
 
 const loadData = (afterLoadCallback) => {
@@ -77,9 +75,81 @@ const whenDocumentLoaded = (action) => {
 	}
 }
 
+// Choropleth feature
+const style = (feature)=> {
+    function getColor(d) {
+        d = d[0] // TODO: Change for propriety in dataset
+        return d > "W" ? '#5C415D' :
+               d > "T"  ? '#e575bc' :
+               d > "P"  ? '#8bf9b9' :
+               d > "L"  ? '#76f5fc' :
+               d > "H"   ? '#f77059' :
+               d > "E"   ? '#FEB24C' :
+               d > "A"   ? '#64db57' :
+                          '#b076fc';
+    }
+    return {
+        fillColor: getColor(feature.properties.ADMIN),
+        weight: 2,
+        opacity: 1,
+        color: '#4f4f4f',
+        dashArray: '2',
+        fillOpacity: 0.9
+    };
+}
+
+    
+const zoomOnCountry = (map) => {
+    return (event) => {
+        map.fitBounds(event.target.getBounds());
+    }
+}
+
+const reset = (geojson) => {
+    return (event)=> {
+        geojson.resetStyle(event.target);
+    }
+}
+
+const highlightFeature = (layer) =>{
+    layer.setStyle({
+        weight: 3,
+        color: 'white',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    layer.bringToFront();
+}
+
+
+
+const applyChoroplethMap = (map) => {
+    json('/data/countries.geojson')
+    .then(function(data) {
+        var geojsonLayer = L.geoJson(data, {
+            style : style,
+            onEachFeature: function (feature, layer) {
+                layer.on('mouseover', function (e) {
+                    highlightFeature(layer)
+                });
+                layer.on('mouseout', function () {
+                    geojsonLayer.resetStyle(layer);
+                });
+                layer.on('click', function (e) {
+                    map.fitBounds(e.target.getBounds());
+                })
+            }
+        }).addTo(map);
+    })
+}
+
+
+
 whenDocumentLoaded(() => {
     if (!window.isScriptLoaded) {
         let map = initMap()
+        applyChoroplethMap(map)
         loadData((data) => {
             let latLongs = data.map((record) => {
                 return { 
@@ -88,7 +158,9 @@ whenDocumentLoaded(() => {
                     mag: parseFloat(record.Magnitude)
                 }
             })
-            initD3MapLayer(map, latLongs)
+            // Add a svg layer to the map
+	        L.svg().addTo(map);
+            let svgOverlayElement = initD3MapLayer(map, latLongs)
         })
         window.isScriptLoaded = true;
     }
