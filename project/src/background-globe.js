52e095eddfe4ab872cb7ab9d34082ec551f838ce
julie-annej <60227045+julie-annej@users.noreply.github.com>
2023-05-28 13:52:39 +0200

    globe done

--- project/src/background-globe.js
+++ project/src/background-globe.js
@@ -9,9 +9,16 @@ export const initWorld = () => {
     fetch('//unpkg.com/world-atlas/land-110m.json').then(res => res.json())
     .then(landTopo => {
         world.polygonsData(topojson.feature(landTopo, landTopo.objects.land).features)
-            .polygonCapMaterial(new THREE.MeshLambertMaterial({ color: 'darkslategrey', side: THREE.DoubleSide }))
+            .polygonCapMaterial(new THREE.MeshLambertMaterial({ color: 'steelblue', side: THREE.DoubleSide }))
             .polygonSideColor(() => 'rgba(0,0,0,0)');
     });
+    
+    let globeContainer = document.querySelector('#globeViz');
+    let width = globeContainer.clientWidth;
+    let height = globeContainer.clientHeight;
+    world.width([width]);
+    world.height([height]);
+    
     world.controls().autoRotate = true;
     world.controls().autoRotateSpeed = 1.0;
 }
