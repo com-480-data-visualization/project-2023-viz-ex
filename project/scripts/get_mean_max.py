cbe7ea7d63f84cdefc8fa570c177d982bf233a1a
julie-annej <60227045+julie-annej@users.noreply.github.com>
2023-05-30 14:41:40 +0200

    update database date format

---
+++ project/scripts/get_mean_max.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from itertools import groupby
+import json
+from datetime import datetime
+import time
+
+
+def getYear(data):
+    dates = data["Date"]
+    # dates = [Date(date) for date in dates]
+    return [date.split("/")[-1] for date in dates] 
+
+def toFloat(data):
+    return [float(x) for x in data["Magnitude"]]
+
+def get_magnitudes_per_country_per_year(data):
+    data = data.assign(Year=lambda x: getYear(x))
+    data = data.assign(Magnitude=lambda x:toFloat(x))
+    df = pd.DataFrame(data, columns=['Date','Time','Latitude','Longitude','Type','Depth','Depth Error','Depth Seismic Stations','Magnitude','Magnitude Type','Magnitude Error','Magnitude Seismic Stations','Azimuthal Gap','Horizontal Distance','Horizontal Error','Root Mean Square','ID','Source','Location Source','Magnitude Source','Status','Country', 'Year'])
+    meanMaxByYear = (df.groupby(['Year','Country'])["Magnitude"].apply(list)).to_dict()
+    
+    newDict = {}
+    for tuple_key, value in meanMaxByYear.items():
+        (year, country) = tuple_key
+        if(newDict.get(year) != None):
+            newDict[year].update({country: value})
+        else :
+            newDict.update({year:{country: value}})
+    
+    with open('./../src/data/earthquake_magnitudes.json', 'w', encoding='utf-8') as jsonf: 
+        json.dump(newDict,jsonf, indent=4)
+    
+
+if __name__ == "__main__":
+    data = pd.read_csv('./../src/data/database.csv')
+    get_magnitudes_per_country_per_year(data)
