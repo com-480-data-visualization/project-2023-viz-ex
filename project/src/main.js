57bf20565a1c49d92a89220f0129ceb1f88edd11
Olivier Saint-Cyr <saintcyr.olivier1@gmail.com>
2023-05-31 15:41:06 +0200

    merge density-plot

--- project/src/main.js
+++ project/src/main.js
@@ -2,6 +2,7 @@ import { csv } from "d3";
 import { initTimeEarthQuakeMap, addDataToTimeMap, TIME_QUAKES_MAP_ID } from "./map/time-map";
 import { COUNTRY_EARTHQUAKES_MAP_ID, addDataToCountryMap, initCountryEarthQuakeMap } from "./map/country-map";
 import { initWorld } from "./background-globe.js";
+import { initDensity } from "./map/density-plot";
 
 const loadData = (afterLoadCallback) => {
   csv(`${document.baseURI}data/database.csv`).then((data) => {
@@ -21,6 +22,7 @@ const whenDocumentLoaded = (action) => {
 whenDocumentLoaded(() => {
   if (!window.isScriptLoaded) {
     let globe = initWorld();
+    initDensity();
     let timeMap = initTimeEarthQuakeMap(TIME_QUAKES_MAP_ID);
     let countryMap = initCountryEarthQuakeMap(COUNTRY_EARTHQUAKES_MAP_ID);
     loadData((data) => {
